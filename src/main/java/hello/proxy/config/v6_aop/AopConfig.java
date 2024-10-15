6e1b2ddde89108c0ea798fb30afe81ea7563510b
seonhoshin <sunlike0508@gmail.com>
2024-10-15 22:58:44 +0900

    Aspect

---
+++ src/main/java/hello/proxy/config/v6_aop/AopConfig.java
@@ -0,0 +1,27 @@
+package hello.proxy.config.v6_aop;
+
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.app.v1.OrderControllerV1Impl;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderRepositoryV1Impl;
+import hello.proxy.app.v1.OrderServiceV1;
+import hello.proxy.app.v1.OrderServiceV1Impl;
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v6_aop.aspect.LogTraceAspect;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV2Config.class, AppV1Config.class})
+public class AopConfig {
+
+    @Bean
+    public LogTraceAspect logTraceAspect(LogTrace logTrace) {
+        return new LogTraceAspect(logTrace);
+    }
+
+}
